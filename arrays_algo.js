10a5bf7cad65380e6abb7261c2c051b3b4a330c6
Adam Wong <33409914+adamjkwong@users.noreply.github.com>
2021-01-24 16:58:36 -0800

    Add files via upload

---
+++ arrays_algo.js
@@ -0,0 +1,38 @@
+var testArr = [6,3,5,1,2,4];
+
+/*
+1. Print each array value and the sum so far
+2. The expected output will be: 
+*/
+
+/*
+var sum = 0;
+for(var i = 0; i < testArr.length; i++){
+    testArr[i]
+    sum= sum + testArr[i]
+    console.log("Num:" + testArr[i] + "," + "Sum:" + sum);
+}
+*/
+
+/*
+Num 6, Sum 6
+Num 3, Sum 9
+Num 5, Sum 14
+Num 1, Sum 15
+Num 2, Sum 17
+Num 4, Sum 21
+*/
+
+/*
+1. Multiply each value in the array by its array position
+2. The expected output will be:
+*/
+
+for(var i = 0; i < testArr.length; i++){
+    testArr[i] = i * testArr[i];
+    console.log(testArr[i]);
+}
+
+/*
+[0,3,10,3,8,20]
+*/
